6d43f2a32265645789ef5ac6a29eebc91fb8d4bc
bamford <stephen.bamford@res-ltd.com>
2018-03-23 06:22:15 +0000

    Added simple classes.

---
+++ ChangeCoinSet.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Change
+{
+    class ChangeCoinSet
+    {
+        readonly List<int> changeCoinValues;
+
+        public ChangeCoinSet()
+        {
+            changeCoinValues = new List<int>();
+        }
+
+        public int CoinSetSize
+        {
+            get
+            {
+                return changeCoinValues.Count;
+            }
+        }
+
+        public int[] CoinSetValuesAsArray
+        {
+            get
+            {
+                return changeCoinValues.ToArray();
+            }
+        }
+
+        public void AddCoinValue(int coinValue)
+        {
+            changeCoinValues.Add(coinValue);
+        }
+
+    }
+}
